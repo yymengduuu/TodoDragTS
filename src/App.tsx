a4c13328022dc33473c6c7b15b7c001c3678161f
yymengduuu <yymengduuu@gmail.com>
2025-06-03 16:00:49 +0100

    Triggering post-commit test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello Vite + React + TypeScript!</div>;
+  return <div>Hello React + TypeScript!</div>;
 }
 
 export default App;
